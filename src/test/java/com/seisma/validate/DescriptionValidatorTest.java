ccc4901fae7b64f0ef441f27c1e122c9b3e3420a
Milinda Bandara <dev.milinda@gmail.com>
2022-09-27 11:36:27 +1000

    add JUnit test cases

---
+++ src/test/java/com/seisma/validate/DescriptionValidatorTest.java
@@ -0,0 +1,55 @@
+package com.seisma.validate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.seisma.dao.Topic;
+import com.seisma.main.TopicRuleFactory;
+
+public class DescriptionValidatorTest{
+	
+	private TopicRuleFactory configTopic = TopicRuleFactory.getInstance();
+	
+	@Test
+	@DisplayName("Test Topic-A Desc more than 10")
+	void validateTopicAMoreDesc10() {
+		
+		String topicId = "A";
+		String topicName = "a";
+		String topicDesc = "12345678910111213";
+		
+		Topic topicA = new Topic(topicId, topicName, topicDesc);
+		
+		assertEquals(true,new DescriptionValidator().validate(topicA));
+	}
+	
+	@Test
+	@DisplayName("Test Topic-A Desc less than 10")
+	void validateTopicADesc10() {
+		
+		String topicId = "A";
+		String topicName = "a";
+		String topicDesc = "1234";
+		
+		Topic topicA = new Topic(topicId, topicName, topicDesc);
+		
+		assertEquals(false,new DescriptionValidator().validate(topicA));
+	}
+	
+	@Test
+	@DisplayName("Test Topic-B Desc less than 40")
+	void validateTopicBDesc10() {
+		
+		String topicId = "A";
+		String topicName = "a";
+		String topicDesc = "1234";
+		
+		Topic topicB = new Topic(topicId, topicName, topicDesc);
+		
+		assertEquals(false,new DescriptionValidator().validate(topicB));
+	}
+
+}
---
+++ src/test/java/com/seisma/validate/NameValidatorTest.java
@@ -0,0 +1,55 @@
+package com.seisma.validate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.seisma.dao.Topic;
+import com.seisma.main.TopicRuleFactory;
+
+public class NameValidatorTest{
+	
+	private TopicRuleFactory configTopic = TopicRuleFactory.getInstance();
+	
+	@Test
+	@DisplayName("Test Topic-A Name")
+	void validateTopicAName() {
+		
+		String topicId = "A";
+		String topicName = "a";
+		String topicDesc = "qwertyuioplkjhg";
+		
+		Topic topicA = new Topic(topicId, topicName, topicDesc);
+		
+		assertEquals(true,new NameValidator().validate(topicA));
+	}
+	
+	@Test
+	@DisplayName("Test Topic-B Name")
+	void validateTopicBName() {
+		
+		String topicId = "B";
+		String topicName = "b";
+		String topicDesc = "qwertyuioplkjhg";
+		
+		Topic topicB = new Topic(topicId, topicName, topicDesc);
+		
+		assertEquals(true,new NameValidator().validate(topicB));
+	}
+	
+	@Test
+	@DisplayName("Test Topic-A Name Incorrect")
+	void validateTopicAIncorrectName() {
+		
+		String topicId = "A";
+		String topicName = "b";
+		String topicDesc = "qwertyuioplkjhg";
+		
+		Topic topicA = new Topic(topicId, topicName, topicDesc);
+		
+		assertEquals(false,new NameValidator().validate(topicA));
+	}
+
+}
